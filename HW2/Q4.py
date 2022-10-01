adc37f430d4c3e447bf35dc037ce470967e042e2
parsaeisa <parsaeisa@github.com>
2022-10-01 23:33:11 +0330

    draw corners

--- HW2/Q4.py
+++ HW2/Q4.py
@@ -6,3 +6,15 @@ path = os.path.join(dirname , 'images/img1.png' )
 
 img = cv2.imread(path ,1)
 print(img.shape)
+
+corner_count_x = 24
+corner_count_y = 17
+
+# mention github link , where you find out what is outputs 
+corners_found , corners = cv2.findChessboardCorners(img , (corner_count_y,corner_count_x) , None )
+
+if corners_found :
+    cv2.drawChessboardCorners(img , (corner_count_y , corner_count_x) , corners , corners_found)
+    cv2.imwrite("image_with_corners.jpg", img)
+else : 
+    print("Corners were not successfully found .")
